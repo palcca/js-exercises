1ba0331818b260573b672acc4e815043bb0c037f
Palcca <dgabor90@gmail.com>
2024-01-15 11:46:58 +0100

    reworked the style of the result and score text

--- sript.js
+++ sript.js
@@ -1,20 +1,20 @@
-
-function getComputerChoice(){
+function game(){
+    function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3)+1;
-    switch(randomNumber){
-        case 1 : return "rock";
-        case 2 : return  "paper";
-        case 3 : return "scissors";
-        default:
+        switch(randomNumber){
+            case 1 : return "rock";
+            case 2 : return  "paper";
+            case 3 : return "scissors";
+            default:
+        }
     }
-}
 
 let playerScore=0;
 let computerScore=0;
 const result = document.querySelector("#result");
 
-function playRound(playerSelection, computerChoice){
-    computerChoice=getComputerChoice();
+    function playRound(playerSelection){
+    let computerChoice=getComputerChoice();
     if (playerSelection === computerChoice){
         result.textContent="It's a tie, try again!";
         return;
@@ -23,11 +23,11 @@ function playRound(playerSelection, computerChoice){
             case "rock":
                 switch (computerChoice){
                     case "paper": 
-                            result.textContent="You lose! "+computerChoice+" beats "+playerSelection;
+                            result.textContent="You lose, "+computerChoice+" beats "+playerSelection + "!";
                             computerScore++;
                             return;
                     case "scissors":
-                            result.textContent="You win! "+playerSelection+" beats "+computerChoice;
+                            result.textContent="You win, "+playerSelection+" beats "+computerChoice + "!";
                             playerScore++;
                             return;
                 }
@@ -35,11 +35,11 @@ function playRound(playerSelection, computerChoice){
             case "paper":
                 switch (computerChoice){
                     case "scissors": 
-                            result.textContent="You lose! "+computerChoice+" beats "+playerSelection;
+                            result.textContent="You lose, "+computerChoice+" beats "+playerSelection + "!";
                             computerScore++;
                             return;
                     case "rock": 
-                            result.textContent= "You win! "+playerSelection+" beats "+computerChoice;
+                            result.textContent= "You win, "+playerSelection+" beats "+computerChoice + "!";
                             playerScore++;
                             return;
                 }
@@ -47,41 +47,44 @@ function playRound(playerSelection, computerChoice){
             case "scissors":
                 switch (computerChoice){
                     case "rock": 
-                            result.textContent= "You lose! "+computerChoice+" beats "+playerSelection;
+                            result.textContent= "You lose, "+computerChoice+" beats "+playerSelection + "!";
                             computerScore++;
                     case "paper": 
-                            result.textContent= "You win! "+playerSelection+" beats "+computerChoice;
+                            result.textContent= "You win, "+playerSelection+" beats "+computerChoice + "!";
                             playerScore++;
                 }
                 break;
         
         }
     }
+    }
+
+
+    const scoreContainer = document.querySelector("#scoreContainer");
+    const btnRock = document.querySelector("#btnRock");
+    btnRock.addEventListener("click", ()=> {
+                playRound("rock");
+                scoreContainer.textContent = 
+                    "Your score: " + playerScore + " ; Computer score: " + computerScore;
+                }
+    );
+
+    const btnPaper = document.querySelector("#btnPaper");
+    btnPaper.addEventListener("click", () => {
+               playRound("paper");
+               scoreContainer.textContent = 
+                   "Your score: " + playerScore + " ; Computer score: " + computerScore;
+               }
+    );
+
+    const btnScissors = document.querySelector("#btnScissors");
+    btnScissors.addEventListener("click", () => {
+                playRound("scissors");
+                scoreContainer.textContent = 
+                    "Your score: " + playerScore + " ; Computer score: " + computerScore;
+                }
+    );
 }
-const scoreContainer = document.querySelector("#scoreContainer");
-const btnRock = document.querySelector("#btnRock");
-btnRock.addEventListener("click", ()=> {
-            playRound("rock");
-            scoreContainer.textContent = 
-                "Your score: " + playerScore + " ; Computer score: " + computerScore;
-            }
-);
 
-const btnPaper = document.querySelector("#btnPaper");
-btnPaper.addEventListener("click", () => {
-            playRound("paper");
-            scoreContainer.textContent = 
-                "Your score: " + playerScore + " ; Computer score: " + computerScore;
-            }
-);
-
-const btnScissors = document.querySelector("#btnScissors");
-btnScissors.addEventListener("click", () => {
-            playRound("scissors");
-            scoreContainer.textContent = 
-                "Your score: " + playerScore + " ; Computer score: " + computerScore;
-            }
-);
-
-
+game();
 
