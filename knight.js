a263d7c6632fdd4982f98907ffe3801287eec375
palcca <155989655+palcca@users.noreply.github.com>
2024-02-01 20:21:55 +0100

    Add files via upload

--- knight.js
+++ knight.js
@@ -1,65 +1,52 @@
-const table = [
-  ["A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8"],
-  ["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"],
-  ["A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6"],
-  ["A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5"],
-  ["A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4"],
-  ["A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3"],
-  ["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"],
-  ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1"],
-];
-
 function getPossibleSteps(arr) {
-  let steps = [];
-  if (arr[0] + 2 <= 7 && arr[1] - 1 >= 0) steps.push(table[arr[0] + 2][arr[1] - 1]);
-  if (arr[0] + 1 <= 7 && arr[1] - 2 >= 0) steps.push(table[arr[0] + 1][arr[1] - 2]);
-  if (arr[0] - 1 >= 0 && arr[1] - 2 >= 0) steps.push(table[arr[0] - 1][arr[1] - 2]);
-  if (arr[0] - 2 >= 0 && arr[1] - 1 >= 0) steps.push(table[arr[0] - 2][arr[1] - 1]);
-  if (arr[0] - 2 >= 0 && arr[1] + 1 <= 7) steps.push(table[arr[0] - 2][arr[1] + 1]);
-  if (arr[0] - 1 >= 0 && arr[1] + 2 <= 7) steps.push(table[arr[0] - 1][arr[1] + 2]);
-  if (arr[0] + 1 <= 7 && arr[1] + 2 <= 7) steps.push(table[arr[0] + 1][arr[1] + 2]);
-  if (arr[0] + 2 <= 7 && arr[1] + 1 >= 0) steps.push(table[arr[0] + 2][arr[1] + 1]);
-  return steps;
+  let steps = [[arr[0]+2, arr[1]-1], [arr[0]+1, arr[1]-2], [arr[0]-1, arr[1]-2],[arr[0]-2, arr[1]-1],
+               [arr[0]-2, arr[1]+1], [arr[0]-1, arr[1]+2], [arr[0]+1, arr[1]+2],[arr[0]+2, arr[1]+1]]
+  let newArr=[];
+  for (let i = 0; i < 8; i++){
+    if(!(steps[i][0] > 7 || steps[i][1] > 7 || steps[i][0] < 0 || steps[i][1] < 0)) newArr.push(steps[i]);
+  }
+
+  return newArr;
 }
+const table = ["A","B","C","D","E","F","G","H"]
 
 function findCoord(string) {
-  for (let i = 0; i < 8; i++) {
-    for (let y = 0; y < 8; y++) {
-       if (string === table[i][y]) return [i, y];
+  let arr = string.split("");
+  arr[1]=Number(arr[1]);
+  for (let i = 0; i < 8; i++){
+    if (arr[0] === table[i]) {
+      
+      return [i, arr[1]]
     }
   }
 }
 
 class LinkedNode {
-  constructor(value, next) {
-    this.value = value;
-    this.next = next;
-  }
+  constructor(value, next) { this.value = value; this.next = next; }
 }
 
 function knight(from, dest) {
-  if (from === dest) {    
-    
+  if (from === dest) {       
     return null;
   }
-  let current = new LinkedNode(from);
-  let queue = [current];
-  let possibleSteps = getPossibleSteps(findCoord(current.value));
-  while (queue[0].value !== dest) {
-    current = queue.shift(0);
-    possibleSteps = getPossibleSteps(findCoord(current.value));
-    for (let i = 0; i < possibleSteps.length; i++) {
+  dest = findCoord(dest);
+  let queue = [new LinkedNode(findCoord(from))];
+  let possibleSteps = getPossibleSteps(queue[0].value);
+  while (queue[0].value.toString() !== dest.toString()) {
+    let current = queue.shift(0);
+    possibleSteps = getPossibleSteps((current.value));
+    for ( i = 0; i < possibleSteps.length; i++) {
       queue.push(new LinkedNode(possibleSteps[i], current));
     }
-  }
-  let array = [queue[0]];
-  current=queue[0];
+  }  
+  let array = [[table[queue[0].value[0]],queue[0].value[1]].join("")];
+  let current=queue[0];
   while (current.next) {
-    array.unshift(current.next);
+    array.unshift([table[current.next.value[0]],current.next.value[1]].join(""));
     current = current.next;
   }
 
   return array;
 }
 
-console.log(knight("C2", "H7"));
+console.log(knight("A1", "H7"))
